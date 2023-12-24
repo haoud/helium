49a49f54bdba2591428bd1135ff524be25f7bd70
Haoud <cadilhac.romain@gmail.com>
2023-12-24 15:40:30 +0100

    User program can now use a "normal" main function
    Instead of directly using the application entry point and manually call the runtime setup

--- user/template/src/main.rs
+++ user/template/src/main.rs
@@ -1,11 +1,7 @@
 #![no_std]
-#![no_main]
 
 extern crate alloc;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    iron::init();
+fn main() {
     iron::syscall::serial::print("Hello, world!\n");
-    iron::syscall::task::exit(iron::syscall::task::id() as i32);
 }
