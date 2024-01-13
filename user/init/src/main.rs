2170be3df051c5117f4e690b8c49ad66ec47b2fa
Haoud <cadilhac.romain@gmail.com>
2024-01-14 00:01:37 +0100

    Removed some code in iron
    Porting rust std will be waaay easier than rewriting everything from scratch... Porting std is not that difficult after a quick glance

--- user/init/src/main.rs
+++ user/init/src/main.rs
@@ -1,21 +1,14 @@
 #![no_std]
-#![allow(internal_features)]
-#![feature(prelude_import)]
 
-#[prelude_import]
-pub use iron::prelude::*;
-
-#[macro_use]
-extern crate iron;
 extern crate alloc;
 
 fn main() {
     let id = iron::syscall::task::spawn("/shell.elf").expect("Failed to load shell");
-    println!("Spawned task with id: {}", id.0);
+    iron::syscall::serial::print(&alloc::format!("Spawned shell with id: {}\n", id.0));
 
     let fd = iron::syscall::vfs::open("/test.txt", iron::syscall::vfs::OpenFlags::MUST_CREATE)
         .expect("Failed to open file");
-    println!("Opened file with fd: {:?}", fd);
+    iron::syscall::serial::print(&alloc::format!("Opened file with fd: {:?}\n", fd));
 
     iron::syscall::vfs::close(fd).expect("Failed to close file");
 }
