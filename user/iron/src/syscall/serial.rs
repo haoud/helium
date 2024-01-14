fa820b5c2dd30db2237d547b53398435976fb41a
Haoud <cadilhac.romain@gmail.com>
2024-01-14 12:26:01 +0100

    Added `serial::read` user
    Does not work since the corresponding syscall does not exist (yet)

--- user/iron/src/syscall/serial.rs
+++ user/iron/src/syscall/serial.rs
@@ -12,3 +12,21 @@ pub fn print(str: &str) {
         );
     }
 }
+
+/// Write data to the serial port.
+pub fn write(data: &[u8]) -> usize {
+    unsafe {
+        core::arch::asm!(
+            "syscall",
+            in("rax") Syscall::SerialWrite as u64,
+            in("rsi") data.as_ptr(),
+            in("rdx") data.len(),
+        );
+    }
+    data.len()
+}
+
+/// Read data from the serial port.
+pub fn read(_: &mut [u8]) -> usize {
+    0
+}
