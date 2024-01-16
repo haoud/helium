d11fc22eb5fecb579eecb30c8b53d852c07611d3
Haoud <cadilhac.romain@gmail.com>
2024-01-16 14:59:18 +0100

    Removed `iron` crate and init now use Rust `std` and `syscall`

--- user/init/install.sh
+++ user/init/install.sh
@@ -10,16 +10,16 @@ die() {
 
 # Verify if debug and release builds coexist. If so, copy the most recent one: it is probably
 # the one that the user wants to run.
-if [ -e target/x86_64/debug/init ] && [ -e target/x86_64/release/init ]; then
-    if [ target/x86_64/debug/init -nt target/x86_64/release/init ]; then
-        cp -v target/x86_64/debug/init ../../iso/boot/init.elf
+if [ -e target/x86_64-unknown-helium/debug/init ] && [ -e target/x86_64-unknown-helium/release/init ]; then
+    if [ target/x86_64-unknown-helium/debug/init -nt target/x86_64-unknown-helium/release/init ]; then
+        cp -v target/x86_64-unknown-helium/debug/init ../../iso/boot/init.elf
     else
-        cp -v target/x86_64/release/init ../../iso/boot/init.elf
+        cp -v target/x86_64-unknown-helium/release/init ../../iso/boot/init.elf
     fi
-elif [ -e target/x86_64/debug/init ]; then
-    cp -v target/x86_64/debug/init ../../iso/boot/init.elf
-elif [ -e target/x86_64/release/init ]; then
-    cp -v target/x86_64/release/init ../../iso/boot/init.elf
+elif [ -e target/x86_64-unknown-helium/debug/init ]; then
+    cp -v target/x86_64-unknown-helium/debug/init ../../iso/boot/init.elf
+elif [ -e target/x86_64-unknown-helium/release/init ]; then
+    cp -v target/x86_64-unknown-helium/release/init ../../iso/boot/init.elf
 else
     die "No init executable found"
 fi
--- user/init/src/main.rs
+++ user/init/src/main.rs
@@ -1,30 +1,3 @@
-#![no_std]
-
-use iron::syscall::*;
-extern crate alloc;
-
 fn main() {
-    task::spawn("/shell.elf").expect("Failed to spawn shell");
-
-    let fd = vfs::open(
-        "/test.txt",
-        vfs::OpenFlags::READ | vfs::OpenFlags::WRITE | vfs::OpenFlags::MUST_CREATE,
-    )
-    .expect("Failed to open file");
-    serial::print(&alloc::format!("Opened file with fd: {:?}\n", fd));
-
-    vfs::write(&fd, "Hello, world !".as_bytes()).expect("Failed to write to file");
-    assert!(vfs::seek(&fd, vfs::Whence::Start(0)) == Ok(0));
-
-    let mut buffer = [0u8; 1024];
-    let bytes_read = vfs::read(&fd, &mut buffer).expect("Failed to read from file");
-    let slice = &buffer[..bytes_read];
-
-    serial::print(&alloc::format!(
-        "Read {} bytes from test.txt: '{}'\n",
-        bytes_read,
-        alloc::string::String::from_utf8(slice.to_vec()).unwrap()
-    ));
-
-    vfs::close(fd).expect("Failed to close file");
+    println!("Hello, std world!");
 }
